391f4bcc66a32770123fca8ff847913690a60518
eliezio.soares <eliezio.soares@sicoob.com.br>
2020-04-15 02:35:28 -0300

    criando segurança

---
+++ src/main/java/com/curso/pontointeligente/api/utils/PasswordUtils.java
@@ -0,0 +1,27 @@
+package com.curso.pontointeligente.api.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(PasswordUtils.class);
+
+    /**
+     * Gera um hash utilizando o BCrypt.
+     *
+     * @param senha
+     * @return String
+     */
+    public static String gerarBCrypt(String senha) {
+        if (senha == null) {
+            log.info("Gerando hash com o BCrypt.");
+            return null;
+        }
+
+        log.info("Gerando hash com o BCrypt.");
+        BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+        return bCryptEncoder.encode(senha);
+    }
+}
